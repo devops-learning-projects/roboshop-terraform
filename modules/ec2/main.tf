961553249c5ce81acb4d95fa09987f358fec21a4
Mainul Islam <mainulislam6168@gmail.com>
2025-09-16 13:07:25 +0600

    env missing

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -38,7 +38,7 @@ resource "null_resource" "ansible" {
     }
 
     inline = [
-      "sudo pip3.11 install ansible",
+      "sudo pip3.11 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/devops-learning-projects/roboshop-ansible roboshop.yml -e role_name=${var.name} -e token=${var.token} -e env=${var.env}"
     ]
   }
