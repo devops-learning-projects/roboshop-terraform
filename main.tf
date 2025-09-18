ea57bccd61fc2e5a8ee564a76dd8dd7a89b7d112
Mainul Islam <mainulislam6168@gmail.com>
2025-09-18 13:12:25 +0600

    increase disk size

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ module "ec2" {
   source        = "./modules/ec2"
   ami           = var.ami
   instance_type = each.value["instance_type"]
+  disk_size = each.value["disk_size"]
   name          = each.key
   env           = var.env
   zone_id       = var.zone_id
--- tools/main.tf
+++ tools/main.tf
@@ -3,7 +3,6 @@ module "tools" {
   source = "../modules/ec2"
   ami = var.ami
   instance_type = each.value["instance_type"]
-  disk_size = each.value["disk_size"]
   name = each.key
   zone_id = var.zone_id
   token = var.token
