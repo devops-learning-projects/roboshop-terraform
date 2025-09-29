85201da8f03bb588f418735b3f20dc3748c3dbdc
Mainul Islam <mainulislam6168@gmail.com>
2025-09-29 09:56:43 +0600

    created aws eks node group and ami role for that

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -32,7 +32,7 @@ resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryPullOn
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
   role       = aws_iam_role.node.name
 }
-
+## IAM Role for eks cluster
 resource "aws_iam_role" "cluster" {
   name = "eks-cluster-example"
   assume_role_policy = jsonencode({
@@ -75,4 +75,52 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSLoadBalancingPolicy"
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSNetworkingPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSNetworkingPolicy"
   role       = aws_iam_role.cluster.name
+}
+
+## IAM Role for EKS Node Group
+resource "aws_iam_role" "example" {
+  name = "eks-node-group-example"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.example.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.example.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.example.name
+}
+
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "nodeg-1"
+  node_role_arn   = aws_iam_role.example.arn
+  subnet_ids      = ["subnet-0a10be74295112b3e", "subnet-0ae77905116bf41c6"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
 }
