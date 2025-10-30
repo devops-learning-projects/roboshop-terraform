711330a17f79c0d6b7e372648a79fa9b57122232
Mainul Islam <mainulislam6168@gmail.com>
2025-10-30 09:37:34 +0600

    set provisioner. after cluster and node groups created ingress will be downloaded by helm.

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -1,4 +1,13 @@
+# added kubeconfig
+resource "null_resource" "kubeconfig" {
+  depends_on = [aws_eks_node_group.main]
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --name ${var.env}"
+  }
+}
+
 resource "helm_release" "nginx_ingress" {
+  depends_on = [null_resource.kubeconfig]
   name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
