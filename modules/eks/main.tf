415209dd64fc22c2141836c5803381bd9a5fe911
Mainul Islam <mainulislam6168@gmail.com>
2025-10-30 12:35:50 +0600

    code dry for addons

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -30,10 +30,10 @@ resource "aws_eks_node_group" "main" {
 
 #  Add Addon for network policy
 resource "aws_eks_addon" "main" {
-  for_each = var.addons
+  for_each     = var.addons
   cluster_name = aws_eks_cluster.main.name
   addon_name   = each.key
-  configuration_values = jsondecode(each.value["config"])
+  configuration_values = jsonencode(each.value["config"])
 }
 
 # cluster access entry for workstation
