aee56c1ff33034a27aa33f59c41ee822b4c88362
Mainul Islam <mainulislam6168@gmail.com>
2025-10-31 20:35:57 +0600

    eabled ingress and set class name

--- provider.tf
+++ provider.tf
@@ -1,7 +1,15 @@
-provider "aws" {}
+provider "aws" {
+  region = "us-east-1"
+}
 
 terraform {
   backend "s3" {}
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.17.0"
+    }
+  }
 }
 
 provider "vault" {
@@ -10,8 +18,25 @@ provider "vault" {
 }
 
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     config_path = "~/.kube/config"
   }
 }
+
+
+# provider "aws" {}
 #
+# terraform {
+#   backend "s3" {}
+# }
+#
+# provider "vault" {
+#   address        = "http://vault-internal.maidevops.fun:8200"
+#   token          = var.token
+# }
+#
+# provider "helm" {
+#   kubernetes = {
+#     config_path = "~/.kube/config"
+#   }
+# }
