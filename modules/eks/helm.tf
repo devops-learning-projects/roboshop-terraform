017df5180f537151a86835f6b42c1f5068d200ce
Mainul Islam <mainulislam6168@gmail.com>
2025-11-04 10:31:57 +0600

    find solution

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -11,7 +11,7 @@ resource "null_resource" "kubeconfig" {
 
 
 resource "helm_release" "nginx_ingress" {
-  depends_on       = [null_resource.kubeconfig]
+  depends_on       = [aws_eks_cluster.main, null_resource.kubeconfig]
   name             = "ingress-nginx"
   repository       = "https://kubernetes.github.io/ingress-nginx"
   chart            = "ingress-nginx"
