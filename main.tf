f53a8f826621c186237ff9cf3e30e497346f3fe0
Mainul Islam <mainulislam6168@gmail.com>
2025-11-24 10:39:35 +0600

    runner

--- main.tf
+++ main.tf
@@ -10,16 +10,15 @@ module "ec2" {
   token         = var.token
 }
 
-## # workstation and eks cluster's PVC should be same
-# module "eks" {
-#   for_each    = var.eks
-#   source      = "./modules/eks"
-#   env         = var.env
-#   eks_version = each.value["eks_version"]
-#   subnet_ids  = each.value["subnet_ids"]
-#   node_groups = each.value["node_groups"]
-#   access      = each.value["access"]
-#   addons      = each.value["addons"]
-#   vault_token = var.token
-# }
-# off
+# # workstation and eks cluster's PVC should be same
+module "eks" {
+  for_each    = var.eks
+  source      = "./modules/eks"
+  env         = var.env
+  eks_version = each.value["eks_version"]
+  subnet_ids  = each.value["subnet_ids"]
+  node_groups = each.value["node_groups"]
+  access      = each.value["access"]
+  addons      = each.value["addons"]
+  vault_token = var.token
+}
