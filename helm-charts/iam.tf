e6c3328cb772bc29c205f4ef6a1e86abb97051ec
Mainul Islam <mainulislam6168@gmail.com>
2025-11-30 08:51:50 +0600

    set up Prometheus EC2 Discovery policy

--- helm-charts/iam.tf
+++ helm-charts/iam.tf
@@ -42,3 +42,49 @@ resource "aws_eks_pod_identity_association" "external-dns" {
   service_account = "external-dns"
   role_arn        = aws_iam_role.external-dns.arn
 }
+
+
+#  Prometheus EC2 Discovery
+resource "aws_iam_role" "prometheus-server" {
+  name = "${var.env}-pod-prometheus-server-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "prometheus-server"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = [
+            "ec2:DescribeInstances",
+            "ec2:DescribeInstanceStatus"
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+}
+
+resource "aws_eks_pod_identity_association" "prometheus-server" {
+  cluster_name    = var.env
+  namespace       = "tools"
+  service_account = "kube-prometheus-stack-prometheus"
+  role_arn        = aws_iam_role.prometheus-server.arn
+}
