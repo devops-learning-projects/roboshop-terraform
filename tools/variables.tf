5c8d916dc80aec6e51ae13a17df4489efebed8e8
Mainul Islam <mainulislam6168@gmail.com>
2025-12-09 16:23:56 +0600

    install packages for elk

--- tools/variables.tf
+++ tools/variables.tf
@@ -19,7 +19,7 @@ variable "tools" {
     elk = {
       instance_type  = "m8i.xlarge"
       spot           = true
-      spot_max_price = 0.14200000000000002
+      spot_max_price = 0.142001
       subnet         = "subnet-0a10be74295112b3e"
     }
   }
