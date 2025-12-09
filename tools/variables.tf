0aa1adcf8be491cf13bd594455c350a7e0f72797
Mainul Islam <mainulislam6168@gmail.com>
2025-12-09 07:58:14 +0600

    set spot_max_price

--- tools/variables.tf
+++ tools/variables.tf
@@ -19,7 +19,7 @@ variable "tools" {
     elk = {
       instance_type  = "m8i.xlarge"
       spot           = true
-      spot_max_price = 0.0930
+      spot_max_price = 0.0775
       subnet         = "subnet-0a10be74295112b3e"
     }
   }
