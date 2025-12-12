b2edc8ea34e728811a795d20ab7680c9248b2df9
Mainul Islam <mainulislam6168@gmail.com>
2025-12-12 11:54:46 +0600

    modified

--- tools/variables.tf
+++ tools/variables.tf
@@ -19,7 +19,7 @@ variable "tools" {
     elk = {
       instance_type  = "m8i.xlarge"
       spot           = true
-      spot_max_price = 0.14240000000000003
+      spot_max_price = 0.0900
       subnet         = "subnet-0a10be74295112b3e"
     }
   }
