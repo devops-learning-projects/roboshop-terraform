b65b84b02cf4a771784a12469c6c984c3f9f0f0f
Mainul Islam <mainulislam6168@gmail.com>
2025-12-16 07:35:50 +0600

    filebeat install on helm chart

--- helm-charts/helm.tf
+++ helm-charts/helm.tf
@@ -130,16 +130,16 @@ resource "helm_release" "cluster-autoscaler" {
   }
 }
 
-## Filebeat Helm Chart
-# resource "helm_release" "filebeat" {
-#   name       = "filebeat"
-#   repository = "https://helm.elastic.co"
-#   chart      = "filebeat"
-#   namespace  = "kube-system"
-#   wait       = "false"
-#
-#   values = [
-#     file("${path.module}/helm-values/filebeat.yml")
-#   ]
-#
-# }
+# Filebeat Helm Chart
+resource "helm_release" "filebeat" {
+  name       = "filebeat"
+  repository = "https://helm.elastic.co"
+  chart      = "filebeat"
+  namespace  = "kube-system"
+  wait       = "false"
+
+  values = [
+    file("${path.module}/helm-values/filebeat.yml")
+  ]
+
+}
