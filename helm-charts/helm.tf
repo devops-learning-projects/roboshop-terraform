b73cfaa4048d51488838210be13fa356e6ac1e65
Mainul Islam <mainulislam6168@gmail.com>
2025-12-19 07:32:36 +0600

    modified

--- helm-charts/helm.tf
+++ helm-charts/helm.tf
@@ -29,6 +29,7 @@ resource "helm_release" "external-dns" {
   create_namespace = true
 }
 
+#
 # Argo CD helm is used to install and manage Argo CD
 resource "helm_release" "argocd" {
   depends_on       = [null_resource.kubeconfig, helm_release.nginx_ingress]
