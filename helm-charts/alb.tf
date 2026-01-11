a5a784d345a184f4566b67d3f4bd4452ca323e7c
Mainul Islam <mainulislam6168@gmail.com>
2026-01-11 07:48:27 +0600

    redirect to https

--- helm-charts/alb.tf
+++ helm-charts/alb.tf
@@ -75,4 +75,19 @@ resource "aws_lb_listener" "https" {
   }
 }
 
+# redirect to https
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
 
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
