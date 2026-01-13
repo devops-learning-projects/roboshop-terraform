bd3057f9e48a88e04afaaf7e69144640fa2bf026
Mainul Islam <mainulislam6168@gmail.com>
2026-01-13 07:14:49 +0600

    set nlb_ips

--- helm-charts/data.tf
+++ helm-charts/data.tf
@@ -24,3 +24,21 @@ data "aws_subnets" "public_subnets" {
     values = ["public-az1", "public-az2"]
   }
 }
+
+# interfaces  for  nlb
+data "aws_network_interfaces" "nlb_interfaces" {
+  filter {
+    name = "interface-type"
+    values = ["network_load_balancer"]
+  }
+}
+
+data "aws_network_interface" "nlb_ips" {
+  count = length(data.aws_network_interfaces.nlb_interfaces.ids)
+  id    = data.aws_network_interfaces.nlb_interfaces.ids[count.index]
+}
+
+output "nlb" {
+  value = data.aws_network_interface.nlb_ips[*].private_ip
+}
+
